6c6f42004f094931bcb60f22dd346adc6a3dd8ea
Krupanandam Muvvala <127400538+krupa128@users.noreply.github.com>
2026-07-25 16:32:15 +0530

    Update ChargemanagerApplication.java

--- src/main/java/com/zynetic/ev/chargemanager/ChargemanagerApplication.java
+++ src/main/java/com/zynetic/ev/chargemanager/ChargemanagerApplication.java
@@ -2,13 +2,14 @@ package com.zynetic.ev.chargemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling // required for OcppService's stale-charger heartbeat check
 public class ChargemanagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChargemanagerApplication.class, args);
-		System.out.println(Thread.activeCount());
 	}
 
 }
